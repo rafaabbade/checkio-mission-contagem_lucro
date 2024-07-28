72758e35777d01c3a0f07020113659cc3c3aabce
Rafael Abbade <rafaelvabbade@gmail.com>
2024-07-28 18:48:06 -0300

    Initial commit from template

--- verification/tests.py
+++ verification/tests.py
@@ -10,6 +10,15 @@ Each test is a dict with
 
 TESTS = {
     "Basics": [
-        {{TESTES}}
+        [
+    {
+        "input": [[7, 1, 5, 3, 6, 4]],
+        "answer": 5
+    },
+    {
+        "input": [[7, 6, 4, 3, 1]],
+        "answer": 0
+    }
+]
     ]
 }
